a88c8c4dcdda89d5dee7b3259f28b008ff5ec81d
Nirvanik <nirvanik77@gmail.com>
2026-06-19 09:33:50 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,82 @@
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.to(".peliculas-bg", {
+  "background-color": "#ffc640",
+    scrollTrigger: {
+      trigger: "#pelicula-2013",
+      start: "top center",
+      end: "bottom center",
+      markers: false,
+      toggleActions: "restart none restart pause"
+    },
+});
+gsap.to(".peliculas-bg", {
+    "background-color": "#00c2ff",
+    immediateRender: false,
+    scrollTrigger: {
+      trigger: "#pelicula-2014",
+      start: "top center",
+      end: "bottom center",
+      markers: false,
+      toggleActions: "restart none restart pause"
+    },
+});
+gsap.to(".peliculas-bg", {
+    "background-color": "#2C286F",
+    immediateRender: false,
+    scrollTrigger: {
+      trigger: "#pelicula-2015",
+      start: "top center",
+      end: "bottom center",
+      markers: false,
+      toggleActions: "restart none restart pause"
+    }
+});
+gsap.to(".peliculas-bg", {
+  "background-color": "#1C3338",
+  immediateRender: false,
+  scrollTrigger: {
+    trigger: "#pelicula-2016",
+    start: "top center",
+    end: "bottom center",
+    markers: false,
+    toggleActions: "restart none restart pause"
+  }
+});
+gsap.to(".peliculas-bg", {
+  "background-color": "#7A788C",
+  immediateRender: false,
+  scrollTrigger: {
+    trigger: "#pelicula-2017",
+    start: "top center",
+    end: "bottom center",
+    markers: false,
+    toggleActions: "restart none restart pause"
+  }
+});
+gsap.to(".peliculas-bg", {
+  "background-color": "#36A165",
+  immediateRender: false,
+  scrollTrigger: {
+    trigger: "#pelicula-2018",
+    start: "top center",
+    end: "bottom center",
+    markers: false,
+    toggleActions: "restart none restart pause"
+  }
+});
+gsap.to(".peliculas-bg", {
+  "background-color": "#000000",
+  immediateRender: false,
+  scrollTrigger: {
+    trigger: "#pelicula-2000",
+    start: "top center",
+    end: "bottom center",
+    markers: false,
+    toggleActions: "restart none restart pause"
+  }
+});
+
+
+
+   
